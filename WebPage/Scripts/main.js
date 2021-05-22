43cc75bdfa408d3e9e1412ef7ed52248ae4275f0
Andres M <andres.mejia@tecnicopascualbravo.edu.co>
2021-05-22 17:52:40 -0500

    Webpage main files and struct

---
+++ WebPage/Scripts/main.js
@@ -0,0 +1,11 @@
+function hiddeMenu() {
+    // document.getElementById("fh5co-aside").innerHTML = "";
+    document.getElementById("fh5co-aside").classList.toggle('active');
+    // var menu = document.getElementById("menu-icon");
+    // if (menu.style.color == "black") {
+    //     menu.style.color = "white";
+    // }
+    // else {
+    //     menu.style.color = "black";
+    // }
+}
